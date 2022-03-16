106949cd8534e212df7eb75a338500f8b872f4ea
Max Zalubovskiy <44732505+viplash4@users.noreply.github.com>
2022-03-16 09:51:19 +0200

    base add

---
+++ src/screens/IndexScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Text, View, StyleSheet } from 'react-native';
+
+
+const IndexScreen = () => {
+    return <View>
+        <Text>IndexScreen</Text>
+    </View>
+};
+
+const style = StyleSheet.create({
+    
+});
+
+
+export default IndexScreen;
