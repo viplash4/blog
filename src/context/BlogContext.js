abf29ca733f229cd184d4ecf67fd10e66689ee84
Max Zalubovskiy <44732505+viplash4@users.noreply.github.com>
2022-03-17 17:55:34 +0200

    useReducer work

--- src/context/BlogContext.js
+++ src/context/BlogContext.js
@@ -3,12 +3,20 @@ import React, { useReducer} from 'react';
 const BlogContext = React.createContext();
 
 const blogReducer = (state, action) => {
-
+    switch (action.type) {
+        case 'add_blogpost':
+            return [...state, { title: `Blog post #${state.length + 1}`}]
+        
+        default:
+            return;
+    }
 };
 
 export const BlogProvider = ({ children }) => {
     const [blogPosts, dispatch] = useReducer(blogReducer, []);
-
+    const addBlogPost = () => {
+        dispatch({type : 'add_blogpost'});
+    };
     
     return (<BlogContext.Provider value={{data: blogPosts, addBlogPost}}>
         {children}
